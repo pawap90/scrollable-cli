24f2124281ab479eaedea3e22c086c59d8a3a603
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-07-21 19:06:04 -0300

    add a third box

--- examples/2-keypress-events.ts
+++ examples/2-keypress-events.ts
@@ -1,5 +1,6 @@
 import { emitKeypressEvents } from 'node:readline';
 import { Scrollable } from '../src/index';
+import chalk from 'chalk';
 
 console.clear();
 
@@ -8,9 +9,9 @@ const size = { width: 15, height: 6 };
 const catBox = new Scrollable({
     content: `
      /\\_/\\
-    ( o.o )
+    (${chalk.greenBright(' o') + chalk.red('.') + chalk.greenBright('o ')})
 `,
-    start: { x: 4, y: 2 },
+    start: { x: 6, y: 2 },
     size,
     wrapOptions: { trim: false }
 })
@@ -19,20 +20,37 @@ const catBox = new Scrollable({
 
 const cowBox = new Scrollable({
     content: `
-    ((...))
+    ${chalk.yellow('((...))')}
     ( O O )
      \\   /
-     (\`_\`)
+     ${chalk.magenta('(`_`)')}
 `,
-    start: { x: 24, y: 2 },
+    start: { x: 30, y: 2 },
     size,
     wrapOptions: { trim: false }
+}).print();
+
+const storyBox = new Scrollable({
+    content:
+        'The cat and the cow where playing hide and seek. The cat was hiding in the barn, but the cow was too big to fit inside. \n' +
+        'The cow was hiding in the field, but the cat was too small to see over the grass. \n' +
+        'The cat was looking for the cow, and the cow was looking for the cat. \n' +
+        'They stumbled upon a ' + chalk.magenta('magical portal') + ' in the middle of the field. ' +
+        'Curiosity getting the best of them, they jumped in together. ' +
+        'Inside, they found a talking mushroom that offered them three wishes each. ' +
+        'The cat wished for a jetpack, the cow wished for wings, and they zoomed off into the sky. ' +
+        'Soon, they encountered a UFO piloted by alien chefs who challenged them to a cooking competition. ' +
+        'The cat whipped up intergalactic lasagna, and the cow produced flying milkshakes. ' +
+        'The judges declared them winners, and they were crowned ' +
+        chalk.italic.yellow('rulers of the galaxy.'),
+    start: { x: 5, y: 10 },
+    size: { width: 40, height: 3 }
 }).print();
 
 printBoxBorders(catBox);
 printBoxBorders(cowBox);
 
-process.stdout.cursorTo(0, 10);
+process.stdout.cursorTo(0, 15);
 
 console.log('Up [↑] and down [↓] arrow keys to scroll. [Q] to quit');
 
@@ -46,12 +64,14 @@ process.stdin.on('keypress', (str, key) => {
     if (key.name == 'up') {
         cowBox.scroll(-1).print();
         catBox.scroll(1).print();
-        process.stdout.cursorTo(0, 10);
+        storyBox.scroll(1).print();
+        process.stdout.cursorTo(0, 15);
     }
     if (key.name == 'down') {
         cowBox.scroll(1).print();
         catBox.scroll(-1).print();
-        process.stdout.cursorTo(0, 10);
+        storyBox.scroll(1).print();
+        process.stdout.cursorTo(0, 15);
     }
     if (key.name == 'q') {
         process.exit();
@@ -61,7 +81,7 @@ process.stdin.on('keypress', (str, key) => {
 function printBoxBorders(box: Scrollable) {
     const { stdout } = process;
     const { size, start } = box.options;
-    
+
     const height = size.height + 2;
     const width = size.width + 2;
     const x = start.x - 1;
@@ -71,7 +91,7 @@ function printBoxBorders(box: Scrollable) {
     console.log(Array(width).fill('─').join(''));
     stdout.cursorTo(x, y + height - 1);
     console.log(Array(width).fill('─').join(''));
-    
+
     for (let i = 0; i < height - 1; i++) {
         stdout.cursorTo(x, y + i);
         console.log('│');
