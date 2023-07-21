8994b7bf6ba1c9f2d635de9026962eb155a06e37
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-07-21 19:05:34 -0300

    use alias

--- examples/1-simple-box.ts
+++ examples/1-simple-box.ts
@@ -1,19 +1,26 @@
 import { setTimeout } from 'timers/promises';
+import chalk from 'chalk';
 import Scrollable from '../src/index';
-
-const box = Scrollable()
-    .setContent(
-        'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
-    )
-    .setStart({ x: 10, y: 3 })
-    .setSize({ width: 20, height: 5 });
 
 console.clear();
 
 // Print background.
 for (let i = 0; i < 10; i++) {
-    console.log(Array(40).fill('.').join(''));
+    console.log(Array(42).fill('.').join(''));
 }
+
+const box = Scrollable()
+    .setContent(
+        "Lorem ipsum is boring but I couldn't think of anything else, so here's a yellow cat:" +
+        chalk.yellow(`
+        |\\---/|
+        | o_o |
+        `)
+    )
+    .setStart({ x: 12, y: 3 })
+    .setSize({ width: 22, height: 4 })
+    .setWrapOptions({ trim: false, hard: false, wordWrap: true })
+    .print();
 
 (async () => {
     await autoscroll('down');
@@ -23,8 +30,9 @@ for (let i = 0; i < 10; i++) {
 })();
 
 async function autoscroll(direction: 'up' | 'down') {
-    for (let i = 0; i < 8; i++) {
-        box.print().scroll(direction == 'up' ? -1 : 1);
-        await setTimeout(1000);
+    await setTimeout(500);
+    for (let i = 0; i < 6; i++) {
+        box.scroll(direction == 'up' ? -1 : 1).print();
+        await setTimeout(500);
     }
 }
